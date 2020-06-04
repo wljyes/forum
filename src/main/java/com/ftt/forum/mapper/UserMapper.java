f4b01093e47ccbc033f2ec042e335fda5a8ee224
wljyes <1757280079@qq.com>
2020-06-04 19:34:07 +0800

    update UserMapper

--- src/main/java/com/ftt/forum/mapper/UserMapper.java
+++ src/main/java/com/ftt/forum/mapper/UserMapper.java
@@ -1,15 +1,26 @@
 package com.ftt.forum.mapper;
 
 import com.ftt.forum.entity.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserMapper {
 
-    @Insert({"insert into user (name, password) values (#{name}, #{password})"})
+    @Insert("insert into user (name, password) values (#{name}, #{password})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void insert(User user);
+
+
+    @Delete("delete from user where id = #{id}")
+    void delete(int id);
+
+    @Update("update user set name = #{name}, password = #{password} where id = #{id}")
+    void update(User user);
+
+    @Select("select id, name, password from user where id = #{id}")
+    void selectById(int id);
+
+    @Select("select * from user where name = #{name}")
+    User selectByName(String name);
 }
