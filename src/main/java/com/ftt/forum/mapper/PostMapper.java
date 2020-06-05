f9b66dbb307f8d49e4d0ba99d490f91319aacb7f
wljyes <1757280079@qq.com>
2020-06-05 11:01:06 +0800

    update PostMapper

--- src/main/java/com/ftt/forum/mapper/PostMapper.java
+++ src/main/java/com/ftt/forum/mapper/PostMapper.java
@@ -5,24 +5,25 @@ import com.ftt.forum.entity.User;
 import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostMapper {
 
-    @Insert("insert into post (id, uid, create_date, update_date, content) values (#{id}, #{uid}, #{create_date}, #{update_date}, #{content})")
+    @Insert("insert into post (uid, create_date, update_date, content) values (#{uid}, #{create_date}, #{update_date}, #{content})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void insert(Post post);
 
 
     @Delete("delete from post where id = #{id}")
-    void delete(Integer id);
+    void delete(int id);
 
-    @Update("update post set name = #{name}, password = #{password} where id = #{id}")
-    void update(User user);
+    @Update("update post set update_date = #{update_date} where id = #{id}")
+    void updateDate(Post post);
 
-    @Select("select id, name, password from user where id = #{id}")
-    Post selectById(Integer id);
+    @Select("select * from post where id = #{id}")
+    Post selectById(int id);
 
-    @Select("select * from user where name = #{name}")
-    User selectByName(String name);
-
+    @Select("select * from post order by update_date desc")
+    List<Post> selectList();
 }
